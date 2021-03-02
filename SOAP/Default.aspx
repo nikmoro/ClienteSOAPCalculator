1b325563ffce483bcf066d5d4f6aa0032d654c60
nikmoro <chilparosales@gmail.com>
2021-03-01 20:24:15 -0600

    Agregar archivos de proyecto.

---
+++ SOAP/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SOAP.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Cliente SOAP</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Num1: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            Num2: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            Resultado: <asp:TextBox ID="TextBoxResult" runat="server" ></asp:TextBox><br />
+          
+            <br /><asp:Button ID="Button1" runat="server" Text="Suma" OnClick="Button1_Click" /><br />
+            <br /><asp:Button ID="Button2" runat="server" Text="Resta" OnClick="Button2_Click" /><br />
+            <br /><asp:Button ID="Button3" runat="server" Text="Multiplicación" OnClick="Button3_Click" /><br />
+            <br /><asp:Button ID="Button4" runat="server" Text="División" OnClick="Button4_Click" /><br />
+
+            <%--<br /><asp:Label ID="Label1" runat="server" Text="Resultado"></asp:Label>--%>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SOAP/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SOAP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        // Objeto que hace uso del Web service
+        CalculatorWebService.CalculatorSoapClient oCalculator = new CalculatorWebService.CalculatorSoapClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBoxResult.Text = oCalculator.Add(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBoxResult.Text = oCalculator.Subtract(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            TextBoxResult.Text = oCalculator.Multiply(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            TextBoxResult.Text = oCalculator.Divide(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+    }
+}
