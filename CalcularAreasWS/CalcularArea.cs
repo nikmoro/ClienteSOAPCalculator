91ca4ce14eb26b1259dd2687c360135fd5d7d502
nikmoro <chilparosales@gmail.com>
2021-03-05 19:43:46 -0600

    WSTerminado

---
+++ CalcularAreasWS/CalcularArea.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularAreasWS
+{
+    public class CalcularArea  // Cuadrado, Triángulo y Círculo
+    {
+        public double ladoBase { get; set; }
+        public double ladoAltura { get; set; }
+        public double radio { get; set; }
+
+        public double AreaCuadrado(double lado)
+        {
+            lado = ladoBase;
+
+            return ladoBase * ladoBase;
+        }
+
+        public double AreaTriangulo(double _base, double altura)
+        {
+            _base = ladoBase;
+            altura = ladoAltura;
+
+            return (ladoBase * ladoAltura) / 2;
+        }
+
+        public double AreaCirculo(double r)
+        {
+            r = radio;
+
+            return (Math.PI * Math.Pow(radio,2));
+        }
+    }
+}
---
+++ CalcularAreasWS/CalcularAreasWebService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalcularAreasWS
+{
+    /// <summary>
+    /// Descripción breve de CalcularAreasWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularAreasWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public double AreaCuadrado(double lado)
+        {
+            CalcularArea oCal = new CalcularArea();
+            return oCal.AreaCuadrado(lado);
+        }
+
+        [WebMethod]
+        public double AreaTriangulo(double _base, double altura)
+        {
+            CalcularArea oCal = new CalcularArea();
+            return oCal.AreaTriangulo(_base, altura);
+        }
+
+        [WebMethod]
+        public double AreaCirculo(double r)
+        {
+            CalcularArea oCal = new CalcularArea();
+            return oCal.AreaCirculo(r); ;
+        }
+    }
+}
